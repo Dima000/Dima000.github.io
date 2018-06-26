faeb696d3bed42a8151a584832632f74157ceea1
Dumitru Motpan <dumitru.motpan@digital1.online>
2018-06-26 19:14:59 +0300

    work experience section

--- src/app/content/work-experience/WorkExperience.js
+++ src/app/content/work-experience/WorkExperience.js
@@ -1,63 +1,78 @@
 import React, {Component} from 'react';
-import './style.css';
+import './work-experience.css';
 
 export class WorkExperience extends Component {
   render() {
-    const data = {
-      title: "Work Experience (Volar Doeheris)",
-      startDate: "July 2015",
-      periods: [
-        {
-          company: "ISDC | Endava*",
-          position: "Full Stack Developer",
-          start: "Oct 2016",
-          end: "Sep 2017",
-          points: [
-            "I made the transition to full Front End/AngularJS developer and started working on a new project.",
-            "Worked together with a designer and was responsible for the behavioral part of the interface and communication with Back End.",
-            "Was promoted to Developer position after 1 year. I continued working on the same project, but in a smaller team.",
-            "Write code and discuss development strategies with colleagues",
-            "Perform code review"
-          ],
-          remark: "*ISDC was bought by Endava in the beginning of 2017"
-        },
-        {
-          company: "ISDC",
-          position: "Junior Full Stack Developer",
-          start: "July 2015",
-          end: "Sep 2016",
-          points: [
-            "After internship I entered a Java Spring/AngularJS project with a team of 7.",
-            "Write Java End Points and SQL queries all with Unit Tests",
-            "Write FE views using AngularJS, HTML/CSS",
-            "Work using Agile Methodology"
-          ]
-        }
-      ]
-    };
+    const {entries} = this.props;
 
-    return (
-        <div className="w3-container w3-card-2 w3-white w3-margin-bottom">
-          <h2 className="w3-text-grey padding-16-top">
-            <i className="fa fa-laptop fa-fw w3-margin-right w3-xxlarge w3-text-teal"/>
-            {data.title}
-            <span className="pull-right">2+ years</span>
-          </h2>
-          {data.periods.map((item, index) => (
-              <div key={index} className="w3-container">
-                <h5 className="w3-opacity"><b>{item.company}</b></h5>
-                <h6 className="w3-text-teal">
-                  <i className="fa fa-calendar fa-fw w3-margin-right"/>
-                  {item.start} - <span className="w3-tag w3-teal w3-round">{item.end}</span>
-                </h6>
-                <ul>
-                  {item.points.map((text, key) => <li key={key}>{text}</li>)}
-                </ul>
-                <hr/>
-              </div>)
-          )}
-        </div>
+    return <div className="work-section" id="work">
+      <h2 className="title-area">
+        <span className="mr-3">{this.props.title}</span>
+        <i className="fa fa-certificate mr-3 color-main"/>
+      </h2>
 
-    );
+      <div className="section-content">
+        {entries.map((entry, key) =>
+            <div key={key} className="job-entry">
+
+              <h4 className="opacity job-title mb-2">
+                <span>{entry.position}</span>
+                <span> @</span>
+                <span className="bold">{entry.company}</span>
+              </h4>
+
+              <h5 className="color-main job-period mb-3">
+                <i className="fa fa-calendar mr-3"/>
+                <span>{entry.start}</span>
+                <span> - </span>
+                <span>{entry.end}</span>
+              </h5>
+
+              <ul className="job-bullet-points">
+                {entry.bullets.map((text, key) =>
+                    <li key={key}>{text}</li>
+                )}
+              </ul>
+
+              {entry.remark && <p className="remark">{entry.remark}</p>}
+            </div>
+        )}
+      </div>
+    </div>;
   }
 }
+
+WorkExperience.defaultProps = {
+  title: "Work Experience",
+  startDate: "July 2015",
+  entries: [
+    {
+      company: "Digital1",
+      position: "Front End Developer",
+      start: "Oct 2017",
+      end: "June 2018",
+      bullets: [
+        "After internship I entered a project about stock market index in a team of 7 people",
+        "Worked under Agile Methodology with monthly sprints and code review",
+        "Got comfortable with Java Spring, SQL, AngularJs and some jQuery",
+        "Was promoted to Developer position after 1 year. I continued working on the same project, but in a smaller team.",
+        "After 1,5 years switched project and started working as full Front-End developer in AngularJs"
+      ],
+      remark: "*ISDC was bought by Endava in the beginning of 2017"
+    },
+    {
+      company: "ISDC | Endava",
+      position: "Full Stack Java Developer",
+      start: "July 2015",
+      end: "Sep 2017",
+      bullets: [
+        "After internship I entered a project about stock market index in a team of 7 people",
+        "Worked under Agile Methodology with monthly sprints and code review",
+        "Got comfortable with Java Spring, SQL, AngularJs and some jQuery",
+        "Was promoted to Developer position after 1 year. I continued working on the same project, but in a smaller team.",
+        "After 1,5 years switched project and started working as full Front-End developer in AngularJs"
+      ],
+      remark: "*ISDC was bought by Endava in the beginning of 2017"
+    }
+  ]
+};
