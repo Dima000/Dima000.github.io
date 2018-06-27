899298a503487eaf375052f065cbc9a6ae831e9a
Dumitru Motpan <dumitru.motpan@digital1.online>
2018-06-27 15:03:56 +0300

    Work experience customize the template

--- src/app/content/work-experience/WorkExperience.js
+++ src/app/content/work-experience/WorkExperience.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactHtmlParser from "react-html-parser";
 import './work-experience.css';
 
 export class WorkExperience extends Component {
@@ -28,11 +29,23 @@ export class WorkExperience extends Component {
                 <span>{entry.end}</span>
               </h5>
 
-              <ul className="job-bullet-points">
-                {entry.bullets.map((text, key) =>
-                    <li key={key}>{text}</li>
-                )}
-              </ul>
+              <div className="milestones">
+                <h6>{entry.milestones.title}</h6>
+                <ul>
+                  {entry.milestones.items.map((text, key) =>
+                      <li key={key}>{ReactHtmlParser(text)}</li>
+                  )}
+                </ul>
+              </div>
+
+              <div className="special-skills">
+                <h6>{entry.specialSkills.title}</h6>
+                <ul>
+                  {entry.specialSkills.items.map((text, key) =>
+                      <li key={key}>{ReactHtmlParser(text)}</li>
+                  )}
+                </ul>
+              </div>
 
               {entry.remark && <p className="remark">{entry.remark}</p>}
             </div>
@@ -51,27 +64,46 @@ WorkExperience.defaultProps = {
       position: "Front End Developer",
       start: "Oct 2017",
       end: "June 2018",
-      bullets: [
-        "After internship I entered a project about stock market index in a team of 7 people",
-        "Worked under Agile Methodology with monthly sprints and code review",
-        "Got comfortable with Java Spring, SQL, AngularJs and some jQuery",
-        "Was promoted to Developer position after 1 year. I continued working on the same project, but in a smaller team.",
-        "After 1,5 years switched project and started working as full Front-End developer in AngularJs"
-      ],
-      remark: "*ISDC was bought by Endava in the beginning of 2017"
+      milestones: {
+        title: "Milestones",
+        items: [
+          "Started working with <b>Angular4</b> on an outsourcing project on retail",
+          "Active collaboration with german colleagues and 1 more person in our office",
+          "In february switched to <a href='https://www.visitor-analytics.io'>Visitor Analytics</a> project and worked with <b>React</b>, <b>Redux</b> in a local team",
+        ],
+      },
+      specialSkills: {
+        title: "Special project skills",
+        items: [
+          "Asynchronous flows with <a href='https://redux-saga.js.org'><b>redux-saga</b></a>",
+          "Development of cross-platform email templates",
+          "Interaction with designer tools such as <a href='https://www.invisionapp.com'>InVision</a> and <a href='https://zeplin.io'>Zeplin</a>"
+        ]
+      }
     },
     {
       company: "ISDC | Endava",
       position: "Full Stack Java Developer",
       start: "July 2015",
       end: "Sep 2017",
-      bullets: [
-        "After internship I entered a project about stock market index in a team of 7 people",
-        "Worked under Agile Methodology with monthly sprints and code review",
-        "Got comfortable with Java Spring, SQL, AngularJs and some jQuery",
-        "Was promoted to Developer position after 1 year. I continued working on the same project, but in a smaller team.",
-        "After 1,5 years switched project and started working as full Front-End developer in AngularJs"
-      ],
+      milestones: {
+        title: "Milestones",
+        items: [
+          "After internship I entered a project about stock market index in a team of 7 people",
+          "Worked under Agile Methodology with monthly sprints and code review",
+          "Got comfortable with <b>Java Spring</b>, <b>SQL</b>, <b>AngularJs</b> and <b>jQuery</b>",
+          "Was promoted to Developer position after 1 year. I continued working on the same project, but in a smaller team.",
+          "After 1,5 years switched project and started working as full Front-End developer in AngularJs"
+        ]
+      },
+      specialSkills: {
+        title: "Special project skills",
+        items: [
+          "Worked on a complex project with huge amount of functionality",
+          "Wrote extensive queries using SQL",
+          "Strict respect of the Agile Methodology"
+        ]
+      },
       remark: "*ISDC was bought by Endava in the beginning of 2017"
     }
   ]
